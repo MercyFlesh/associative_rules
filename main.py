99f08537ff09f258b9fc2f7c56aa61ccfda2c9a6
MercyFlesh <con2288@mail.ru>
2020-07-05 03:28:52 +0300

    changed modules path; added new args

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-import csv
 import argparse
-from apriori import apriori, get_associative_rules
+from assotiative_rules.apriori import apriori
+from assotiative_rules.rules import get_associative_rules
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-P', '--path', help="path to database", type=str, required=True)
-    parser.add_argument('-S', '--support', type=float, default=0.5, required=False)
-    parser.add_argument('-C', '--confidience', type=float, default=0.6, required=False)
+    parser.add_argument('-S', '--sup', help="minimum support", type=float, required=True)
+    parser.add_argument('--conf', help="minimum confidience", type=float, required=False)
+    parser.add_argument('--lift', help="minimum lift", type=float, required=False)
+    parser.add_argument('--levar', help="minimum levarage", type=float, required=False)
+    parser.add_argument('--conv', help="minimum conviction", type=float, required=False)
     args = parser.parse_args()
 
     try:
-        sets_satisfying_min_sup = apriori(args.path, args.support) 
-        rules = get_associative_rules(args.path, sets_satisfying_min_sup, args.confidience)
+        support_items = apriori(args.path, args.sup)
+        rules = get_associative_rules(support_items, 
+                                    conf=args.conf, lift=args.lift, 
+                                    levar=args.levar, conv=args.conv)
+        print(rules)
 
     except Exception as ex:
         print(ex.args[0])
